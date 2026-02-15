4da02a1cf80e43f293b13ac85c9cea630b13b3c8
2025aa05603-create <2025aa05603@wilp.bits-pilani.ac.in>
2026-02-15 09:52:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ if uploaded_file is not None:
         # TRAIN-TEST SPLIT
         # ===============================
         X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.2, random_state=42
+            X, y, test_size=0.3, random_state=42
         )
 
         # ===============================
