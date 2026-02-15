74c4083ee05c6666e8244cd2cf3f888747f7cb51
2025aa05603-create <2025aa05603@wilp.bits-pilani.ac.in>
2026-02-15 12:21:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ if uploaded_file is not None:
                 model = GaussianNB()
                 model.fit(X_train, y_train)
                 y_pred = model.predict(X_test_scaled)
-                y_prob = model.predict_proba(X_test_scaled)
+                y_prob = model.predict_proba(X_test_scaled)[:, 1]
 
             elif model_name == "Random Forest":
                 model = RandomForestClassifier(
